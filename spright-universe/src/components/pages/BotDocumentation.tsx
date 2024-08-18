9030d2f73549c9d46ca8f71594a6e81dc230ab61
Wallace Freitas <103138773+Baku-Stark@users.noreply.github.com>
2024-08-17 23:45:35 -0300

    update : `Bot Page`

--- spright-universe/src/components/pages/BotDocumentation.tsx
+++ spright-universe/src/components/pages/BotDocumentation.tsx
@@ -3,7 +3,13 @@ import { LinkButton } from "../layouts/buttons/LinkButton";
 
 import { SectionContainer } from "../layouts/container/SectionContainer";
 
+import commands from "../../../commands.node.json";
+
+import styles from "./css/BotDocumentation.module.css"
+
 export function BotDocumentation(){
+    const commandList = commands;
+
     return(
         <>
             <Helmet>
@@ -18,6 +24,32 @@ export function BotDocumentation(){
                                 text="Página Inicial"
                             />
                         </aside>
+                    </section>
+                    <hr/>
+                    <section>
+                        <div>
+                            <h2>
+                                <i className="bi bi-caret-left-fill"></i>
+                                Comandos do Imu-Sama
+                                <i className="bi bi-caret-right-fill"></i>
+                            </h2>
+                        </div>
+                        <div className={styles.list_commands}>
+                            <ul>
+                                {
+                                    commandList.map((item, index) => (
+                                        <li key={index} className="fs-5">
+                                            <span className={styles.command_name}>
+                                                /{item['command']}
+                                            </span>
+                                            <p className={`${styles.command_desc} mb-0`}>
+                                                {item['desc']}
+                                            </p>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                        </div>
                     </section>
                 </>
             }></SectionContainer>
